afd69fbcf1d5d2a208f2bb719beb8104df659cb1
darksun27 <sbsiddharth@gmail.com>
2019-10-04 04:47:27 +0530

    Update Initial Sorting

--- FolderAutomation/organise.js
+++ FolderAutomation/organise.js
@@ -1,27 +1,34 @@
-var fs = require('fs');
-var totalFiles = 0;
-var paths = require('./paths.json')["pathData"];
-var pathToWatch = process.argv[2];
+const fs = require('fs');
+const paths = require('./paths.json')["pathData"];
 
-async function countFiles() {
+
+var pathToWatch = process.argv[2];
+var totalFiles = 0;
+var movingFile = false;
+var workingFile = "";
+var extension = "";
+
+ function countFiles() {
     return new Promise((resolve, reject) => {
         var getFileCount = new Promise(function(resolve, reject){
             fs.readdir(`${pathToWatch}`, (err,files) => {
                 if(err) {
+                    console.log("Path Reading Error")
                     reject(err);
                 }
-                resolve(files.length);
+                resolve({"length" : files.length, "filenames" : files});
             })
         })
         getFileCount.then((len) => {
             resolve(len);
         }).catch((err) => {
+            console.Error("Can't Get File Count!");
             reject(err);
         });
     })
 }
 
-async function checkDir(pathToCheck) {
+ function checkDir(pathToCheck) {
     return new Promise((resolve, reject) => {
         fs.exists(`${pathToCheck}`,(exists) => {
             if(!exists) {
@@ -38,12 +45,12 @@ async function checkDir(pathToCheck) {
     })
 }
 
-async function moveFile(extension, filename) {
+ function moveFile(extension, filename) {
     return new Promise((resolve, reject) => {
         checkDir(`${pathToWatch}/${paths[extension]}`).then(() => {
             fs.rename(`${pathToWatch}/${filename}`, `${pathToWatch}/${paths[extension]}/${filename}`, (err) => {
                 if (err) {
-                    reject(err);
+                    reject("Error Moving File\n",err);
                 }
                 resolve();
             })
@@ -53,58 +60,71 @@ async function moveFile(extension, filename) {
     }); 
 }
 
-countFiles().then((filesPresent) => {
-    totalFiles = filesPresent;
+countFiles().then((files) => {
+    totalFiles = files["length"];
     console.log("Total Files Present in the Folder: ", totalFiles);
+    files["filenames"].forEach(file => {
+        workingFile = file.split('.');
+        if(workingFile.length > 1) {
+            extension = workingFile.pop();
+            if(paths[extension]) {
+                movingFile = true;
+                moveFile(extension, file).then(() => {
+                    //console.log("File Moved");
+                    movingFile = false;
+                }).catch((err) => {
+                    console.Error(err);
+                    movingFile = false;
+                });
+            }
+        }
+    });
 }).catch((err) => {
-    console.log(err);
+    console.Error(err);
 });
-
 
 fs.watch(`${pathToWatch}`, (eventType, filename) => {
     if(filename) {
         //console.log("Event Occured: ", eventType," File Changed: ",filename);
-        countFiles().then((filesPresent) => {
-            if(filesPresent > totalFiles) {
-                console.log("add");
-                var extension = filename.split('.').pop();
-                console.log(extension)
-                if(paths[extension]){
+        countFiles().then((files) => {
+            if(files["length"] > totalFiles) {
+                workingFile = filename.split('.');
+                extension = workingFile.pop();
+                if(paths[extension] && !movingFile && workingFile.length > 1){
                     try {
                         moveFile(extension, filename).then(() => {
                             console.log("File Moved")
                         }).catch((err) => {
-                            throw err;
+                            console.log(err)
                         })
                     }
                     catch(err) {
-                        console.log(err);
+                        console.Error("Adding Error\n",err);
                     }
                 }
             }
-            else if( filesPresent < totalFiles) {
+            else if( files["length"] < totalFiles) {
                 console.log("delete");
             }
             else { 
-                console.log("other");
-                var extension = filename.split('.').pop();
-                console.log(extension);
-                if(paths[extension]){
+                workingFile = filename.split('.');
+                extension = workingFile.pop();
+                if(paths[extension] && !movingFile && workingFile.length > 1){
                     try {
                         moveFile(extension, filename).then(() => {
                             console.log("File Moved")
                         }).catch((err) => {
-                            throw err;
+                            console.log(err)
                         })
                     }
                     catch(err) {
-                        console.log(err);
+                        console.Error("Renaming Error\n",err);
                     }
                 }
             }
-            totalFiles = filesPresent;
+            totalFiles = files["length"];
         }).catch((err) => {
-            console.log(err);
+            console.Error(err);
         });
     }
 });
